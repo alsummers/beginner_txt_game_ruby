67ba65d87deb4eb87aa8c01a36be8dfab8ed2f82
Allison Summers <allisonsummers44@gmail.com>
2018-12-21 22:31:48 -0500

    finish

--- game.rb
+++ game.rb
@@ -1,94 +1,122 @@
 class Main
-    # start class
-    # for regex analyze rubular
+
     def initialize()
-        @current_rm = nil #starting room
-        @inventory = []
-        @rooms = {}
+      @current_room = nil 
+      @inventory = []
+      @rooms = {}
     end
-
-    def fill_rooms
-        start_room = {description: "The Main Hall",
-        exits: [["E", 2, /^([\w]+ )e$|^([\w]+ )east$|e$|^east$/i]], #command direction and room where it takes you
-        actions:[["wave", nil]], items: ["barrel"]}
-
-        second_room = {description: "The second room", exits:[["W", 1, /^([\w]+ )w$|^([\w]+ )west$|w$|^west$/i]], actions:[["wave", nil], ["hit", "barrel"]], items: []}
-
-        @rooms = {}
-        @rooms[1] = start_room
-        @rooms[2] = second_room
-        return start_room
+    
+    def populate_rooms
+      
+      start_room = {description:"The Main Room.", exits:[["E", 2, /^([\w]+ )e$|^([\w]+ )east$|^e$|^east$/i]], actions:[["wave", nil]], items:["barrel"]}
+      second_room = {description:"The Second Room.", exits:[["W", 1,/^([\w]+ )w$|^([\w]+ )west$|^w$|^west$/i]], actions:[["wave", nil], ["hit","barrel"]], items:[]}
+      
+      @rooms = {}
+      
+      @rooms[1] = start_room
+      @rooms[2] = second_room
+      
+      return start_room
     end
-
-    def display_room
-        item_string = @current_rm[:items].length > 0 ? "Items: " + @current_rm[:items].join(", ") : ""
-        puts
-        puts @current_rm[:description] + "\n" + item_string + "Exits: " + @current_rm[:exits].map{|x| x[0]}. #<----block code
-        join(", ")
+    
+    def get_room_display    
+      item_string = @current_room[:items].length > 0 ? "Items: " + @current_room[:items].join(", ") + "\n"  : ""
+      @current_room[:description] + "\n" + item_string + "Exits: " + @current_room[:exits].map{|x| x[0] }.join(", ")
     end
-
+    
     def parse_input(input)
-        result = parse_exits(input)
-       return result unless result.nil?
-
-       return "Unrecognized command"
+      
+      result = parse_exits(input)
+      return result unless result.nil?
+      
+      result = parse_actions(input)
+      return result unless result.nil?    
+      
+      return "Invalid command."
     end
-
+    
     def get_item(item)
-        items = @current_rm[:items]
-        if items.contains?(item)
-            @inventory.push(item)
-            items.delete(item)
-            return "You pickup #{item}."
-        end
-        return nil
+      #puts "room_actions(#{item})"
+      items = @current_room[:items]
+      if items.find_index(item)
+        @inventory.push(item)
+        items.delete(item)
+        return "You pickup #{item}."
+      end
+      return "Couldn't find #{item}."
     end
-
+    
+    def room_actions(input)
+      puts "room_actions(#{input})"
+      action = @current_room[:actions].select{|x| input == x[0] }[0]
+      puts "found action #{action}"
+      if not action.nil?       
+        return "You wave!" if action[0] == "wave"
+        if action[0] == "hit"
+          return "You don't have a #{action[1]}." unless @inventory.find_index(action[1])
+          @inventory.delete(action[1])
+          return "You hit the barrel."
+        end
+      end
+      return nil
+    end
+    
+    def get_inventory_display    
+      return "You don't have anything!" if @inventory.length == 0
+      return "Inventory: " +@inventory.join(", ")
+    end
+    
     def parse_actions(input)
-        return display_room if input =~ /^look$/i
-
-        if input =~ /^get ([\w])+/i
-            result = get_item($1)
-            return result unless result.nil?
-        end
-        return nil
+      #puts "parse_actions(#{input})"
+      return get_room_display if input =~ /^look$/i
+      
+      return get_item($1) if input =~ /^get ([\w]+)/i        
+      
+      return get_inventory_display if input =~ /^inv$/i
+      
+      return result = room_actions(input) if input =~ /^[\w]+$/i   
+    
+      return nil
     end
-
+    
     def parse_exits(input)
-        exits = @current_rm[:exits]
-        exit = exits.select{|x| input =~ x[2] }
-        if exit.length > 1
-         return "cant understand direction"
-        end
-        if exit.length == 1
-         @current_rm = @rooms[exit[0][1]]
-         return display_room
-        end
-        return ""
+      exits = @current_room[:exits]
+      exit = exits.select{|x| input =~ x[2] }
+      if exit.length > 1
+        return "cannot read direction"
+      end
+      if exit.length == 1
+        @current_room = @rooms[exit[0][1]]      
+        return get_room_display
+      end
+      return nil
     end
-
+    
     def start()
-        while(run)do
-        end
-        puts "Thanks for playing."
+      while(run) do
+      end    
+      puts "Thanks for playing."
     end
-
-    def run()
-        if @current_rm == nil
-            @current_rm = fill_rooms
-            puts display_room
-        end
-        print "\n>"
-        input = gets.chomp()
-        response = parse_input input
-        puts response
-
-        return false if input =~ /^exit.*/i
-
-        return true
+    
+    def run()    
+      if @current_room == nil 
+        @current_room = populate_rooms 
+        puts get_room_display
+      end
+      print "\n>"
+      input = gets.chomp()
+      
+      return false if input =~ /^exit.*/i
+      
+      response = parse_input input
+      puts response
+      
+      return true
     end
-end
-
-txt_game = Main.new
-
-txt_game.start
+  
+  end
+  
+  
+  a_game = Main.new
+  
+  a_game.start
